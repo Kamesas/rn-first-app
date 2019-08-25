b3ca917657dcb2dc2145c2744a1f80e972165414
Kamesas <semakaleksandr2014@gmail.com>
2019-08-25 20:26:08 +0300

    show items with scroll

--- App.js
+++ App.js
@@ -1,20 +1,76 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView
+} from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [courseGoals, setCoursGoals] = useState([]);
+
+  handleChangeInputGoal = e => {
+    setEnteredGoal(e);
+  };
+
+  addGoal = () => {
+    enteredGoal.trim() !== "" &&
+      setCoursGoals(prevGoals => [...prevGoals, enteredGoal]);
+    setEnteredGoal("");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Opened!</Text>
+    <View style={styles.appWrapper}>
+      <View style={styles.addFormWrapper}>
+        <TextInput
+          placeholder="Enter todo"
+          style={styles.textInputStyle}
+          value={enteredGoal}
+          onChangeText={handleChangeInputGoal}
+        />
+        <Button title="Add" style={styles.addButton} onPress={addGoal} />
+      </View>
+
+      <ScrollView>
+        {courseGoals.map((goal, i) => (
+          <View style={styles.listItem} key={i}>
+            <Text>{goal}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+  appWrapper: {
+    padding: 20,
+    paddingVertical: 30
+  },
+  addFormWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  textInputStyle: {
+    width: "80%",
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 5
+    //flexGrow: 1
+  },
+  addButton: {
+    padding: 50,
+    backgroundColor: "#aaa"
+  },
+  listItem: {
+    padding: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    marginVertical: 2
   }
 });
